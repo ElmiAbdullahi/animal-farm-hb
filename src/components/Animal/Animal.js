94ab5652645d43ba634a82db4c4455cb5c3a71b2
Abdullahi Elmi <abdullahielmi147@gmail.com>
2023-01-09 15:33:56 -0800

    got animals to render on page

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,11 @@
 import animals from '../../data.js';
+import './Animal.css';
 
 export default function Animal(props) {
-  return <></>;
+  return (
+    <div className="animal" style={{ top: props.top, left: props.left }}>
+      <img src={`/animals/${props.type}.svg`} />
+      <h2>{props.name}</h2>
+    </div>
+  );
 }
