167dbb0fe9ec6bdac9ec4862e38218e2864f26df
Abdullahi Elmi <abdullahielmi147@gmail.com>
2023-01-09 15:02:45 -0800

    background image is rendered into page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import Animal from '../Animal/Animal.js';
+import background from '../../background.png';
+
 import './Main.css';
+import { animals } from '../../data.js';
 
 export default function Main() {
-  return <main></main>;
+  return (
+    <main style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal
+          key={animal.name}
+          name={animal.name}
+          type={animal.type}
+          says={animal.says}
+          left={animal.left}
+          top={animal.top}
+        />
+      ))}
+    </main>
+  );
 }
